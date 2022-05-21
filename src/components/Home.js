bf04a349a92dc3c98e6b56304f04987a0ff921c7
oerd bej <oerdbej@gmail.com>
2022-05-21 20:35:41 +0200

    setting server, making a condition after fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,43 +2,7 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      libri: "Ky eshte libri pare",
-      title: "Neveria",
-      author: "Jean Paul Sartre",
-      year: 1950,
-      id: 1,
-    },
-    {
-      libri: "Ky eshte libri dyte",
-      title: "Libri i shqetesimit",
-      author: "Fernando Pessoa",
-      year: 1970,
-      id: 2,
-    },
-    {
-      libri: "Ky eshte libri trete",
-      title: "Revolta Atlasit",
-      author: "Ayn Rand",
-      year: 1980,
-      id: 3,
-    },
-    {
-      libri: "Ky eshte libri katert",
-      title: "Zahiri",
-      author: "Paulo Coehlo",
-      year: 1990,
-      id: 4,
-    },
-    {
-      libri: "Ky eshte libri peste",
-      title: "Letter to my father",
-      author: "Barack Obama",
-      year: 2000,
-      id: 5,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const [name, setName] = useState("");
 
@@ -48,6 +12,13 @@ const Home = () => {
 
   useEffect(() => {
     console.log("This effect will fire by himself");
+    //fetch => promise => object => object.json()
+    fetch('http://localhost:8000/blogs').then(res => {
+      return res.json();
+    }).then((data)=>{
+      // console.log(data);
+      setBlogs(data);
+    })
   }, []); //empty, or we ca use a state, in order => when the state changes, run the useEffect
 
   const handleDelete = (id) => {
@@ -58,11 +29,14 @@ const Home = () => {
 
   return (
     <div className='blog-list'>
-      <BlogList
+      {/* condition needed: first the data was null. We need condition because we are waiting from fetch data.json(). When we receive the data, we make a condtion, if true, than pass the component with props. */}
+      {
+        blogs &&  <BlogList
         blogs={blogs}
         title='Dream On baby🎈'
         handleDelete={handleDelete}
       />
+      }
       <button onClick={() => handleClick("❤️")}>Heart</button>
       <p>{name} </p> 
     </div>
@@ -70,3 +44,6 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
