2bdf1134a07c97eaec34e876d8994460fe671cc8
oerd bej <oerdbej@gmail.com>
2022-05-21 21:23:57 +0200

    catch errors OK

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,8 @@ const Home = () => {
   const [blogs, setBlogs] = useState(null);
   // will make the spinner if the data fetch is geting time
   const [isPending, setIsPending] = useState(true);
+  // When we catch the error, we store it into a state
+  const [error, setError] = useState(null);
 
   const [name, setName] = useState("");
 
@@ -13,15 +15,29 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log("This effect will fire by himself");
-    //fetch => promise => object => object.json()
-    fetch('http://localhost:8000/blogs').then(res => {
-      return res.json();
-    }).then((data)=>{
-      // console.log(data);
-      setBlogs(data);
-      setIsPending(false)
-    })
+    setTimeout(() => {
+      //fetch => promise => object => object.json()
+      fetch('http://localhost:8000/blogs').then(res => {
+        // connection is OK, but cannot get the endpoint json is falsy or does not exist?!, or other problems. Is the response ok, or rather if !ok if the response is is 
+        if(!res.ok) {
+          throw Error('Could not fetch the data. This is catched by the catch()')
+
+        }
+        console.log(res);
+        return res.json();
+      }).then((data)=>{
+        // console.log(data);
+        setBlogs(data);
+        setIsPending(false)
+        setError(null);
+      })
+      .catch((err) => {
+        // console.log(err.message)
+        // When we get the error, it is not loading data, so we set it to false
+        setIsPending(false);
+        setError(err.message)
+      })
+    }, 1000);
   }, []); //empty, or we ca use a state, in order => when the state changes, run the useEffect
 
 
@@ -30,6 +46,7 @@ const Home = () => {
 
   return ( 
     <div className='home'>
+      {error && <div>{error}</div>}
       {
         isPending && <div> Loading . . . 🎈 </div>
       }
